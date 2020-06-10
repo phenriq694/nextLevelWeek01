e26c47623144db5956f0dcbb303a813d478dda1e
Paulo Henrique <phenriq694@gmail.com>
2020-06-09 23:08:43 -0300

    MOBILE: Getting points data by calling the API  and adding a marker on the map using its longitude and latitude informations.

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -13,8 +13,17 @@ interface Item {
   image_url: string;
 }
 
+interface Point {
+  id: number;
+  name: string;
+  image: string;
+  latitude: number;
+  longitude: number;
+}
+
 const Points = () => {
   const [items, setItems] = useState<Item[]>([]);
+  const [points, setPoints] = useState<Point[]>([]);
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
 
   const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
@@ -46,6 +55,18 @@ const Points = () => {
   useEffect(() => {
     api.get('items').then(response => {
       setItems(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    api.get('points', {
+      params: {
+        city: 'São Paulo', 
+        uf: 'SP',
+        items: [1, 2]
+      }
+    }).then(response => {
+      setPoints(response.data);
     });
   }, []);
 
@@ -89,19 +110,22 @@ const Points = () => {
               longitudeDelta: 0.014,
             }}
           >
-            <Marker
-              style={styles.mapMarker}
-              onPress={hanldeNavigateToDetail}
-              coordinate={{
-                latitude: -23.5553977,
-                longitude: -46.7381793,
-              }}
-            >
-              <View style={styles.mapMarkerContainer}>
-                <Image style={styles.mapMarkerImage} source={{ uri: "https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" }} />
-                <Text style={styles.mapMarkerTitle}>Mercado</Text>
-              </View>
-            </Marker>
+            {points.map(point => (
+              <Marker
+                key={String(point.id)}
+                style={styles.mapMarker}
+                onPress={hanldeNavigateToDetail}
+                coordinate={{
+                  latitude: point.latitude,
+                  longitude: point.longitude,
+                }}
+              >
+                <View style={styles.mapMarkerContainer}>
+                  <Image style={styles.mapMarkerImage} source={{ uri: point.image }} />
+                  <Text style={styles.mapMarkerTitle}>{point.name}</Text>
+                </View>
+              </Marker>
+            ))}
           </MapView>
         )}
       </View>
